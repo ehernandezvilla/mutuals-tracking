7244f9f2edbc271a785dcaa71fb84e1472027430
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-03-13 16:21:45 -0300

    adding chromedriver

--- app.py
+++ app.py
@@ -10,70 +10,71 @@ import os
 import arrow
 import pandas as pd
 from time import sleep
-from pyvirtualdisplay import Display
+# from pyvirtualdisplay import Display
 
 
-display = Display(visible=0, size=(400, 800))
-display.start()
+# display = Display(visible=0, size=(400, 800))
+# display.start()
 
 options = webdriver.ChromeOptions()
-options.add_argument('--disable-extensions')
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
+# options.add_argument('--disable-extensions')
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
-#login flow
 
-#open the webpage
-driver.get("URL")
+#Open the webpage
+driver.get("https://www.santanderassetmanagement.cl/buscador-de-fondos")
 
-#target email
-email = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
-#enter username and password
-email.clear()
-email.send_keys('ID_EMAIL')
+sleep(300)
 
-#target password
-password = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
-#enter and password
-password.clear()
-password.send_keys('PASSW_KEY')
+# #target email
+# email = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+# #enter username and password
+# email.clear()
+# email.send_keys('ID_EMAIL')
 
-
-submit = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-driver.implicitly_wait(20)
-#after_codeclick = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-#We are logged in!
+# #target password
+# password = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+# #enter and password
+# password.clear()
+# password.send_keys('PASSW_KEY')
 
 
-#FLUJO DE DESCARGA 
+# submit = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+# driver.implicitly_wait(20)
+# #after_codeclick = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+# #We are logged in!
 
-#Ingreso a seccion rides
 
-rides = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/app-home-layout/div/app-nav/header/nav/a[4]/span"))).click()
+# #FLUJO DE DESCARGA 
 
-arw = arrow.utcnow()
-today = arw.shift(days=-1).format('DD') 
-today = int(today)
-today = today - 1
-today = str(today)
+# #Ingreso a seccion rides
 
-#Date selection 
+# rides = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/app-home-layout/div/app-nav/header/nav/a[4]/span"))).click()
 
-driver.find_element(By.XPATH, '/html/body/app-root/app-home-layout/div/app-order/app-order-list/div[1]/div/div/div[2]/div[2]/div[1]/mat-form-field[1]/div/div[1]/div[2]/mat-datepicker-toggle/button').click()
+# arw = arrow.utcnow()
+# today = arw.shift(days=-1).format('DD') 
+# today = int(today)
+# today = today - 1
+# today = str(today)
 
-alldates=driver.find_elements(By.XPATH,'//table[@class="mat-calendar-table"]//div[@class="mat-calendar-body-cell-content"]')
+# #Date selection 
 
-for dateselements in alldates:
-    date=dateselements.text
-    #print(date)
-    if date==today: 
-        dateselements.click()
-        break
+# driver.find_element(By.XPATH, '/html/body/app-root/app-home-layout/div/app-order/app-order-list/div[1]/div/div/div[2]/div[2]/div[1]/mat-form-field[1]/div/div[1]/div[2]/mat-datepicker-toggle/button').click()
 
-#Descarga de documentos
+# alldates=driver.find_elements(By.XPATH,'//table[@class="mat-calendar-table"]//div[@class="mat-calendar-body-cell-content"]')
 
-download = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/app-home-layout/div/app-order/app-order-list/div[1]/div/div/div[2]/div[2]/div[2]/button/span/mat-icon"))).click()
+# for dateselements in alldates:
+#     date=dateselements.text
+#     #print(date)
+#     if date==today: 
+#         dateselements.click()
+#         break
+
+# #Descarga de documentos
+
+# download = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/app-home-layout/div/app-order/app-order-list/div[1]/div/div/div[2]/div[2]/div[2]/button/span/mat-icon"))).click()
